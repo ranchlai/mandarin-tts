bcc808ef7e4b9719462a700f327cbfa1142ac12e
ranch <ranchlai@hotmail.com>
2021-06-19 01:41:10 +0800

    support variable-length speaker scp

--- mtts/text/text_processor.py
+++ mtts/text/text_processor.py
@@ -1,13 +1,16 @@
+from typing import List
+
+import torch
+from torch import Tensor
+
 from mtts.datasets.dataset import Tokenizer
 from mtts.utils.logging import get_logger
-import torch
-from torch import  Tensor
 
-from typing import List
 logger = get_logger(__file__)
-    
+
+
 class TextProcessor():
-    def __init__(self,config):
+    def __init__(self, config):
         conf = config['dataset']['train']
         self.emb_tokenizers = []
         for key in conf.keys():
@@ -16,29 +19,30 @@ class TextProcessor():
                 self.emb_tokenizers += [emb_tok]
                 logger.info('processed emb {}'.format(conf[key]['_name']))
 
-    def _process(self,input:str):
+    def _process(self, input: str):
         segments = input.split('|')
         name = segments[0]
         segments = segments[1:]
         if len(segments) != len(self.emb_tokenizers):
             raise ValueError('Input text and emb_tokensizers are different, {segments}')
-        
+
         seg_lens = [len(s.split()) for s in segments]
         n = max(seg_lens)
-        for k in seg_lens:
-            if k != n and k != 1:
-                raise ValueError(f'Input segments should share the same length, but {k}!={n} for text {input}')
+        # for k in seg_lens:
+        # if k != n and k != 1:
+        #raise ValueError(f'Input segments should share the same length, but {k}!={n} for text {input}')
 
-        segments = [' '.join(s.split()*n) if len(s.split())==1 else s for s in segments]
+        segments = [' '.join((s.split() * n)[:n]) if len(s.split()) != n else s for s in segments]
         token_tensor = []
-        for seg,tokenizer in zip(segments,self.emb_tokenizers):
+        for seg, tokenizer in zip(segments, self.emb_tokenizers):
             tokens = tokenizer.tokenize(seg)
             token_tensor.append(torch.unsqueeze(tokens, 0))
         token_tensor = torch.cat(token_tensor, 0)
-        return name,token_tensor
+        return name, token_tensor
 
-    def __call__(self,input):
+    def __call__(self, input):
         return self._process(input)
+
 
 if __name__ == '__main__':
     import yaml
